969d723657ec36e47d7b06d396fcc206b96b6b38
Jhonatan <jvasconcelosbittencourt@gmail.com>
2025-11-04 21:42:27 -0300

    novas paginas começadas

--- assets/scripts/js.js
+++ assets/scripts/js.js
@@ -4,3 +4,9 @@ btnMobile.addEventListener("click", () => {
   menuMobile.classList.toggle("show");
   btnMobile.classList.toggle("show");
 });
+const anos = document.querySelectorAll(".ano");
+const date = new Date();
+const anoAtual = date.getFullYear();
+for (let i = 0; i < anos.length; i++) {
+  anos[i].innerText = anoAtual - 1994;
+}
